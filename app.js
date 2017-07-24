a92d26ae9ab658e3e38cafbe54eb226e9f7c0e55
SmokedSalmon <jerrysu.jie@gmail.com>
2017-07-24 16:56:46 +1000

    First commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+
+var app = express();
+
+// === Begin to set up handlebars view engine
+var handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+// === End setting up handlebars view engine
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// route to the landing pages for this experiment
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/wavy', function(req, res) {
+    res.render('wavy');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
